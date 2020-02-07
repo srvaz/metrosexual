e62c2f3565b4efd7153ec5f15036b6ea3dcd6b2c
João Pedro Vaz <jonwify@gmail.com>
2020-02-07 18:53:05 -0300

    :recycle: change theme automatically and remove unnecessary comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { setTheme, filterThemes, THEME_TYPE, UI_THEME } from './utils/theme.util';
+import { isDayPreiod } from './utils/timer.util';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "metrosexual" is now active!');
 
-	let setLightTheme = vscode.commands.registerCommand('extension.metrosexualLightTheme', () => {
-		// The code you place here will be executed every time your command is executed
+	const config = vscode.workspace.getConfiguration();
+	let themeType = isDayPreiod() ? THEME_TYPE.LIGHT : THEME_TYPE.DARK;
+	let themeToSet = config.get(`metrosexual.${themeType}`);
 
+	config.update('workbench.colorTheme', themeToSet);
+
+	setInterval(() => {
+		themeToSet = config.get(`metrosexual.${themeType}`);
+		config.update('workbench.colorTheme', themeToSet);
+	}, 60000);
+
+	let setLightTheme = vscode.commands.registerCommand('extension.metrosexualLightTheme', () => {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Light theme is defined!');
-
-		// const config = vscode.workspace.getConfiguration();
-
-		// const lightTheme = vscode.workspace.getConfiguration('metrosexual').get<string>('lightTheme');
 
 		vscode.window.showQuickPick(
 			[...filterThemes(UI_THEME.LIGHT)],
@@ -28,14 +27,8 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	let setDarkTheme = vscode.commands.registerCommand('extension.metrosexualDarkTheme', () => {
-		// The code you place here will be executed every time your command is executed
-
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Dark theme is defined!');
-
-		// const config = vscode.workspace.getConfiguration();
-
-		// const lightTheme = vscode.workspace.getConfiguration('metrosexual').get<string>('lightTheme');
 
 		vscode.window.showQuickPick(
 			[...filterThemes(UI_THEME.DARK)],
@@ -46,5 +39,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(setLightTheme, setDarkTheme);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
