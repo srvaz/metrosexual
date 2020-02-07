c0bc88255d0999b6fea322dcb7fc3f3b6ee5a9e8
João Pedro Vaz <jonwify@gmail.com>
2020-02-07 19:56:27 -0300

    :rotating_light: fix tslint warnings

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,21 @@ import * as vscode from 'vscode';
 import { UI_THEME, chooseTheme, pickThemes } from './utils/theme.util';
 
 export function activate(context: vscode.ExtensionContext) {
-	chooseTheme();
+  chooseTheme();
 
-	setInterval(() => chooseTheme(), 60000);
+  setInterval(() => chooseTheme(), 60000);
 
-	let setLightTheme = vscode.commands.registerCommand('extension.metrosexualLightTheme', () => {
-		pickThemes(UI_THEME.LIGHT, 'Pick your light theme');
-		chooseTheme();
-	});
+  let setLightTheme = vscode.commands.registerCommand('extension.metrosexualLightTheme', () => {
+    pickThemes(UI_THEME.LIGHT, 'Pick your light theme');
+    chooseTheme();
+  });
 
-	let setDarkTheme = vscode.commands.registerCommand('extension.metrosexualDarkTheme', () => {
-		pickThemes(UI_THEME.DARK, 'Pick your dark theme');
-		chooseTheme();
-	});
+  let setDarkTheme = vscode.commands.registerCommand('extension.metrosexualDarkTheme', () => {
+    pickThemes(UI_THEME.DARK, 'Pick your dark theme');
+    chooseTheme();
+  });
 
-	context.subscriptions.push(setLightTheme, setDarkTheme);
+  context.subscriptions.push(setLightTheme, setDarkTheme);
 }
 
 export function deactivate() {}
